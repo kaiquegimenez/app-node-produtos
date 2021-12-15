610649202017c29dff71cd56e6be5359def8bb50
Kaique Gimenez <kaiquesgimenez_sg_@hotmail.com>
2021-12-15 20:27:30 -0300

    conectando com o banco de dados

--- api/routes/apiRouter.js
+++ api/routes/apiRouter.js
@@ -1,16 +1,17 @@
 const express = require('express');
 let apiRouter = express.Router();
 
+const knex = require('knex') ({
+  client: 'pg',
+  connection: process.env.DATABASE_URL
+});
+
 let endpoint = '/'
 apiRouter.get(endpoint + 'produtos', (req, res) => {
-  res.status(200).json([
-    { id: 1, descricao: "Arroz parboilizado 5Kg", valor: 25.00, marca: "Tio João" },
-    { id: 2, descricao: "Maionese 250gr", valor: 7.20, marca: "Helmans" },
-    { id: 3, descricao: "Iogurte Natural 200ml", valor: 2.50, marca: "Itambé" },
-    { id: 4, descricao: "Batata Maior Palha 300gr", valor: 15.20, marca: "Chipps" },
-    { id: 5, descricao: "Nescau 400gr", valor: 8.00, marca: "Nestlé" },
-  ]
-  )
+  knex
+  .select('*')
+  .from('produto')
+  .then(produtos => res.status(200).json(produtos));
 });
 
 module.exports = apiRouter;
